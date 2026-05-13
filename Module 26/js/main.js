11de3567b491d2b2d9038f477d0b95f8a65768da
LekeSefa1 <leke1sefa@gmail.com>
2026-05-13 19:15:15 +0200

    Module 26 and 27

---
+++ Module 26/js/main.js
@@ -0,0 +1,31 @@
+console.log("goodbye!")
+
+var x=5;
+
+var b=20+34;
+
+document.write(b);
+
+var a=99;
+a--;
+console.log(a);
+
+var y=20;
+var q=y**2;
+console.log(q);
+
+var z=12;
+z*=3;
+console.log(z);
+
+var r=5;
+var t=5;
+console.log(r == t);
+
+var u=5;
+var i=4;
+console.log(u != i);
+
+var u=5;
+var i=4;
+console.log(u >= i);
