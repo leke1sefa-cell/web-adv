55a96126f836f505c2a91a8f9b7a78ce40c67216
LekeSefa1 <leke1sefa@gmail.com>
2026-06-01 19:18:44 +0200

    Module 31

---
+++ Module 31/js/main.js
@@ -0,0 +1,30 @@
+var a = 0;
+do {
+    a = a + 1;
+    console.log(a);
+
+}while(a<5);
+
+console.log("------------");
+
+
+var i=0;
+
+while (i>5){
+    i=i+1;
+    console.log(i);
+}
+console.log("-----------------");
+var x=0;//1
+var y=0;//1,2
+
+while(y<4){
+    y++;////y=1,2,3,4
+    x+=y;////x=x+y 1,2+1=3,6,4+6=10
+    console.log(x);
+    console.log(y);
+
+}
+for(b=0;b<5;b++){
+    console.log("vlera e b eshte :" + b)
+}
